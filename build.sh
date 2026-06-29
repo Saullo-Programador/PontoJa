cf2761956773b7a013d2f0e0516f0074c78ec9e7
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2026-06-28 21:19:41 -0300

    Use Flutter 3.41.9 on Vercel

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -e
 
-git clone https://github.com/flutter/flutter.git --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
+git clone https://github.com/flutter/flutter.git \
+  --branch 3.41.9 \
+  --depth 1
+
+export PATH="$PWD/flutter/bin:$PATH"
+
+flutter --version
 
 flutter config --enable-web
+
 flutter pub get
+
 flutter build web --release
